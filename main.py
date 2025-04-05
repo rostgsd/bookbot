26db3e557118b1726f2cc1e82c19330efbfa26f3
rostgsd <ostiandrees@gmail.com>
2025-04-05 15:39:50 -0600

    cleanup

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 
 def get_book_text(file):
     with open(file) as str:
-        return str.read()
-    
-def count_words(text):
-    words = text.split()
-    return len(words)
-
+        return str.read()    
+        
 def main():
+    from stats import count_words
+
     text = get_book_text("/home/rostgsd/workspace/github.com/personal/bookbot/books/frankenstein.txt")
     word_count = count_words(text)
     print(f'{word_count} words found in the document')
--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+
 def count_words(text):
     words = text.split()
     return len(words)
