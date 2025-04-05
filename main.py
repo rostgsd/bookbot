bc93d4300257fcdd130f07f8a1cde8877296b1ae
rostgsd <ostiandrees@gmail.com>
2025-04-05 16:37:17 -0600

    add count characters func

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ def get_book_text(file):
         return str.read()    
         
 def main():
-    from stats import count_words
+    from stats import count_words, count_chars
 
     text = get_book_text("/home/rostgsd/workspace/github.com/personal/bookbot/books/frankenstein.txt")
     word_count = count_words(text)
     print(f'{word_count} words found in the document')
-
+    print(count_chars(text))
+    
 main()
